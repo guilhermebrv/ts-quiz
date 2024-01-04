99cb546eaa5e89c8e5f59578da1ac91ae45cf974
Guilherme Viana <guibviana@icloud.com>
2024-01-04 16:33:44 +0000

    finished leaderboard table view cell

---
+++ TaylorSwiftQuiz/Leaderboard/Table View Cells/LeaderboardTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  LeaderboardTableViewCell.swift
+//  TaylorSwiftQuiz
+//
+//  Created by Guilherme Viana on 03/01/2024.
+//
+
+import UIKit
+
+class LeaderboardTableViewCell: UITableViewCell {
+	static let identifier: String = String(describing: LeaderboardTableViewCell.self)
+	private let screen: LeaderboardTableViewCellView = LeaderboardTableViewCellView()
+	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		addSubview(screen)
+		screen.pin(to: self)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+extension LeaderboardTableViewCell {
+	public func setupCell(player: Player) {
+		let eraColorMap: [String: UIColor] = [
+				"taylor-swift": .taylorswift,
+				"fearless": .fearless,
+				"speak-now": .speaknow,
+				"red": .tsred,
+				"1989": .ts1989,
+				"reputation": .reputation,
+				"lover": .lover,
+				"folklore": .folklore,
+				"evermore": .evermore,
+				"midnights": .midnights]
+		screen.bgView.backgroundColor = eraColorMap[player.era ?? ""]
+		screen.playerNameLabel.text = player.name
+		screen.playerScoreLabel.text = String(player.points)
+		screen.difficultyLabel.text = player.difficulty
+	}
+}
